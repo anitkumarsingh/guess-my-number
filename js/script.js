d95254582b90aa8152fdbf8ba7d75c7fc4f6c57b
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-16 14:47:17 +0530

    dom manipulation

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log(document.querySelector('.msg').textContent);
+document.querySelector('.msg').textContent = 'Correct Number!';
+
+document.querySelector('.score-number').textContent = 20;
+document.querySelector('.high-score').textContent = 10;
+
+document.querySelector('.enter-number').value = 20;
