5c02b8ef34c8a4400576b07d464933d224c18614
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-16 16:54:17 +0530

    minor css manipulation

--- js/script.js
+++ js/script.js
@@ -4,42 +4,44 @@ let score = 20;
 let hightScore = 0;
 
 const computerGuess = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.guessed-number').textContent = computerGuess;
 document.querySelector('.score-number').textContent = score;
 document.querySelector('.high-score').textContent = hightScore;
-
 
 document.querySelector('.btn-check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.enter-number').value);
   if (!guess) {
     document.querySelector('.msg').textContent = ' ⛔ No number!';
-  }else if(guess === computerGuess){
+  } else if (guess === computerGuess) {
+    document.querySelector('.guessed-number').textContent = computerGuess;
     document.querySelector('.msg').textContent = '🎉 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.guessed-number').style.width='16%';
+    document.querySelector('.enter-number').style.backgroundColor ='#60b347'
+    document.querySelector('.guessed-number').style.width = '16%';
     hightScore = score;
-    document.querySelector('.high-score').textContent= hightScore;
-  }else if(guess < computerGuess){
-    if(score > 1){
+    document.querySelector('.high-score').textContent = hightScore;
+  } else if (guess < computerGuess) {
+    if (score > 1) {
       document.querySelector('.msg').textContent = '📉 Too Low!...';
       score--;
       document.querySelector('.score-number').textContent = score;
-    }else{
+    } else {
       document.querySelector('.msg').textContent = '😬 You lost the game';
       document.querySelector('.score-number').textContent = 0;
     }
-  }else if(guess > computerGuess){
-    if(score > 1){
+  } else if (guess > computerGuess) {
+    if (score > 1) {
       document.querySelector('.msg').textContent = '📈 Too High!...';
       score--;
       document.querySelector('.score-number').textContent = score;
-    }else{
+    } else {
       document.querySelector('.msg').textContent = '😬 You lost the game';
       document.querySelector('.score-number').textContent = 0;
     }
   }
 });
 
-document.querySelector('.btn-play-again').addEventListener('click',function(){
-console.log('asdas');
-})
+document
+  .querySelector('.btn-play-again')
+  .addEventListener('click', function () {
+    console.log('asdas');
+  });
