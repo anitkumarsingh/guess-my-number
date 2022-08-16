7dc5e2f244d1707107039f0e17d2554c5409fe51
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-16 15:12:06 +0530

    event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 'use strict';
+/*
 console.log(document.querySelector('.msg').textContent);
 document.querySelector('.msg').textContent = 'Correct Number!';
+
+document.querySelector('.guessed-number').textContent =12;
 
 document.querySelector('.score-number').textContent = 20;
 document.querySelector('.high-score').textContent = 10;
 
 document.querySelector('.enter-number').value = 20;
+
+*/
+
+document.querySelector('.btn-check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.enter-number').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.msg').textContent = ' ⛔ No number';
+  }
+});
